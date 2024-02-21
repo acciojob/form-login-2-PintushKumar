7e84cae1935a25c53aa05304d4b64a0a42545020
Pintush  Kumar <pintushmitcse@gmail.com>
2024-02-21 14:17:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,2 @@
 //your JS code here. If required.
-const fname = document.getElementById('firstName');
-const lname = document.getElementById('lastName');
-const phoneNumber = document.getElementById('phoneNumber');
-const email = document.getElementById('email');
-function submitForm(){
-	  var result = "First Name: " + fname + "\n" +
-                     "Last Name: " + lname + "\n" +
-                     "Phone Number: " + phoneNumber + "\n" +
-                     "Email ID: " + email;
-
-    alert(result);
-}
 
